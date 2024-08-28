59ed8d305ba362d3097248cf5fef23ecd6358d8b
Johan Guttormsson <655558+johangu@users.noreply.github.com>
2024-08-28 11:48:04 +0200

    fix(wezterm): make rectangle work with wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,36 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.term = "wezterm"
+
 config.color_scheme = "Catppuccin Macchiato"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 14
 
 config.enable_tab_bar = false
+
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "RightArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "UpArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "DownArrow",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 config.window_padding = {
 	left = 0,
